1eeae120f92cc51faaa0c72496b880df702ba253
chamincheol <54410332+chamincheol@users.noreply.github.com>
2021-02-04 01:06:45 +0900

    [변경] : boards model에 created_at 추가

--- src/models/boards.ts
+++ src/models/boards.ts
@@ -1,21 +1,24 @@
 import mongoose from "mongoose";
 
-const BoardsSchema = new mongoose.Schema({
-  body: {
-    type: String,
-    required: true,
+const BoardsSchema = new mongoose.Schema(
+  {
+    body: {
+      type: String,
+      required: true,
+    },
+    created_at: {
+      type: Date,
+    },
+    hashtag: {
+      type: [String],
+    },
+    title: {
+      type: String,
+      required: true,
+    },
   },
-  created_at: {
-    type: Date,
-  },
-  hashtag: {
-    type: [String],
-  },
-  title: {
-    type: String,
-    required: true,
-  },
-});
+  { timestamps: { createdAt: "created_at" } },
+);
 
 const Boards = mongoose.model("Boards", BoardsSchema);
 
