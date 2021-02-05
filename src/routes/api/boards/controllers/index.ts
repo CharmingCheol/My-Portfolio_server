038bbc33ff843a5fa9a469091f7a94ee9d3297cb
chamincheol <54410332+chamincheol@users.noreply.github.com>
2021-02-05 19:11:00 +0900

    [추가] : getBoardById Validator 추가

--- src/routes/api/boards/controllers/index.ts
+++ src/routes/api/boards/controllers/index.ts
@@ -1,1 +1,2 @@
 export { default as createBoards } from "./createBoards";
+export { default as getBoardById } from "./getBoardById";
--- src/routes/api/boards/index.ts
+++ src/routes/api/boards/index.ts
@@ -5,5 +5,6 @@ import * as validators from "./validators";
 const router = Router();
 
 router.post("/", validators.createBoard, controllers.createBoards);
+router.get("/:id", validators.getBoardById, controllers.getBoardById);
 
 export default router;
