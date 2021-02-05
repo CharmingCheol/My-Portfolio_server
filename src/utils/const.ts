2e4121a61463bd2c461168293dc674ecc5437e49
chamincheol <54410332+chamincheol@users.noreply.github.com>
2021-02-05 19:42:03 +0900

    [변경] : const.ts 변경

--- src/utils/const.ts
+++ src/utils/const.ts
@@ -1,4 +1,6 @@
-export const DOES_NOT_EXISTS_BODY = "본문을 입력하지 않았습니다";
-export const DOES_NOT_EXISTS_HASH_TAG = "해시태그를 입력하지 않았습니다";
-export const DOES_NOT_EXISTS_TITLE = "제목을 입력하지 않았습니다";
-export const GREATER_THAN_HASH_TAG = "해시태그 입력 갯수를 초과하였습니다";
+export const ERROR_TEST_ID = "asdsad";
+export const OK_TEST_ID = "60196cb62b371e6020088b66";
+export const WRONG_TEST_ID = "60196cb62b371e6020088b65";
+
+export const NOT_UNDERSTAND_ID = "not understand ID"; // mongo가 이해하지 못하는 ID
+export const NOT_FOUND_ID = "not found ID"; // mongo가 이해는 할 수 있지만 존재하지 않는 ID
