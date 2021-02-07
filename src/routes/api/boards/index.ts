a9f3d87da229a1cd78ef2072f17e6660240f16e7
chamincheol <54410332+chamincheol@users.noreply.github.com>
2021-02-07 14:34:26 +0900

    [추가] : checkSpecialSymbols 미들웨어 추가

--- src/routes/api/boards/index.ts
+++ src/routes/api/boards/index.ts
@@ -1,11 +1,18 @@
 import { Router } from "express";
 import { validateErrorHandler } from "@utils/errorHandler";
 import * as controllers from "./controllers";
+import * as middlewares from "@routes/middlewares";
 import * as validators from "./validators";
 
 const router = Router();
 
-router.post("/", validators.createBoard, validateErrorHandler, controllers.createBoards);
+router.post(
+  "/",
+  middlewares.checkSpecialSymbols({ method: "body", key: "category" }),
+  validators.createBoard,
+  validateErrorHandler,
+  controllers.createBoards,
+);
 router.get("/:id", validators.getBoardById, validateErrorHandler, controllers.getBoardById);
 router.get("/", validators.getBoards, validateErrorHandler, controllers.getBoards);
 
