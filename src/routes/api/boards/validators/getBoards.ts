95db06be034fc65ee51c6b85dc1cf208dd4698d7
chamincheol <54410332+chamincheol@users.noreply.github.com>
2021-02-08 01:50:02 +0900

    [추가, 삭제] : getBoards Validator 갱신

--- src/routes/api/boards/validators/getBoards.ts
+++
@@ -1,4 +0,0 @@
-import { query } from "express-validator";
-import { WRONG_PAGE_TYPE } from "@utils/const";
-
-export default [query("page").isNumeric().withMessage(WRONG_PAGE_TYPE).bail().isString().withMessage(WRONG_PAGE_TYPE)];
