7a2b57b5d23bae7201bb18a85e059d31e2d2788a
chamincheol <54410332+chamincheol@users.noreply.github.com>
2021-02-25 01:47:54 +0900

    [수정] : 더미 데이터 수정

--- src/utils/const.ts
+++ src/utils/const.ts
@@ -1,9 +1,10 @@
 export const ERROR_TEST_ID = "asdsad";
-export const OK_TEST_ID = "60196cb62b371e6020088b66";
+export const OK_TEST_ID = "603087449d77ce5acc19816b";
 export const WRONG_TEST_ID = "60196cb62b371e6020088b65";
 
 export const NOT_UNDERSTAND_ID = "not understand ID"; // mongo가 이해하지 못하는 ID
 export const NOT_FOUND_ID = "not found ID"; // mongo가 이해는 할 수 있지만 존재하지 않는 ID
+export const NOT_FOUND_CATEGORY = "not found category";
 
 export const WRONG_PAGE_TYPE = "Page QS must be int type"; // page query string 타입이 string 형태의 int가 아닌 경우
 
