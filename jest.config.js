23b1d4dde1bfbc2f43ffcc2bb0a2b6b45a2265e0
CharmingCheol <toby0806@naver.com>
2022-07-03 13:40:49 +0900

    nestjs 환경 세팅

---
+++ jest.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  testRegex: '.*\\.spec\\.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  collectCoverageFrom: ['**/*.(t|j)s'],
+  coverageDirectory: '../coverage',
+  testEnvironment: 'node',
+  moduleNameMapper: {
+    'controller/(.*)': '<rootDir>/src/controller/$1',
+    'dto/(.*)': '<rootDir>/src/dto/$1',
+    'model/(.*)': '<rootDir>/src/model/$1',
+    '^model': '<rootDir>/src/model/index',
+    'module/(.*)': '<rootDir>/src/module/$1',
+    'service/(.*)': '<rootDir>/src/service/$1',
+    '^utils/(.*)': '<rootDir>/src/utils/$1',
+    '^app.module': '<rootDir>/src/app.module.ts',
+  },
+};
