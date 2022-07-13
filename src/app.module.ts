4ae1ff3b7e5f3e4ec8cfd28062d46a6a942b79fd
CharmingCheol <toby0806@naver.com>
2022-07-13 23:26:25 +0900

    ConfigModule 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,28 @@
 import { APP_FILTER, APP_PIPE } from '@nestjs/core';
 import { Module, ValidationPipe } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 
-import WritingController from 'controller/writing';
-import WritingService from 'service/writing';
 import { typeOrmOptions } from 'model';
 import WritingModule from 'module/writing.module';
+import ImageModule from 'module/image.module';
 import HttpExceptionFilter from 'utils/httpExceptionFilter';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeOrmOptions), WritingModule],
-  controllers: [WritingController],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: process.env.NODE_ENV === 'dev' ? 'dev.env' : 'prod.env',
+      ignoreEnvFile: process.env.NODE_ENV === 'prod',
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string().valid('dev', 'prod').required(),
+      }),
+    }),
+    TypeOrmModule.forRoot(typeOrmOptions),
+    WritingModule,
+    ImageModule,
+  ],
   providers: [
-    WritingService,
     { provide: APP_FILTER, useClass: HttpExceptionFilter },
     { provide: APP_PIPE, useClass: ValidationPipe },
   ],
