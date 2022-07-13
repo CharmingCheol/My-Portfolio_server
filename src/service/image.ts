a4a0f0cef6483699ae9f517457ca1627a2771e47
CharmingCheol <toby0806@naver.com>
2022-07-13 23:39:30 +0900

    ImageService 추가

---
+++ src/service/image.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+class ImageService {
+  constructor(private configService: ConfigService) {}
+
+  uploadImage(filename: string) {
+    const env = this.configService.get<'dev' | 'prod'>('NODE_ENV');
+    if (env === 'dev') {
+      return `http://localhost:3001/images/${filename}`;
+    }
+  }
+}
+
+export default ImageService;
