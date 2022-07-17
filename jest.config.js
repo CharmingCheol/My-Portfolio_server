125be2b1ba42c17748091ea906d791f709244a6d
CharmingCheol <toby0806@naver.com>
2022-07-17 19:29:04 +0900

    jest 환경에서 filter, pipe 파일 절대 경로 지정

--- jest.config.js
+++ jest.config.js
@@ -10,9 +10,11 @@ module.exports = {
   moduleNameMapper: {
     'controller/(.*)': '<rootDir>/src/controller/$1',
     'dto/(.*)': '<rootDir>/src/dto/$1',
+    'filter/(.*)': '<rootDir>/src/filter/$1',
     'model/(.*)': '<rootDir>/src/model/$1',
     '^model': '<rootDir>/src/model/index',
     'module/(.*)': '<rootDir>/src/module/$1',
+    'pipe/(.*)': '<rootDir>/src/pipe/$1',
     'service/(.*)': '<rootDir>/src/service/$1',
     '^utils/(.*)': '<rootDir>/src/utils/$1',
     '^app.module': '<rootDir>/src/app.module.ts',
