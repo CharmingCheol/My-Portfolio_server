f4c34cbc4b84b2f774b769112acb6449705377f8
CharmingCheol <toby0806@naver.com>
2022-07-20 01:10:01 +0900

    dto, model에 ApiProperty 데코레이터 적용

--- src/dto/writing.ts
+++ src/dto/writing.ts
@@ -1,12 +1,15 @@
-import { Writing } from 'types/writing';
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString } from 'class-validator';
+import { Writing } from 'types/writing';
 
 export class WritingRequestDto implements Pick<Writing, 'content' | 'title'> {
   @IsString()
   @IsNotEmpty()
+  @ApiProperty()
   content: string;
 
   @IsString()
   @IsNotEmpty()
+  @ApiProperty()
   title: string;
 }
--- src/model/writing.ts
+++ src/model/writing.ts
@@ -1,19 +1,23 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Entity, Column, PrimaryGeneratedColumn, CreateDateColumn } from 'typeorm';
-
 import { Writing } from 'types/writing';
 
 @Entity()
 class WritingModel implements Writing {
   @CreateDateColumn()
+  @ApiProperty()
   createdAt: Date;
 
   @PrimaryGeneratedColumn('uuid')
+  @ApiProperty()
   id: string;
 
   @Column()
+  @ApiProperty()
   content: string;
 
   @Column()
+  @ApiProperty()
   title: string;
 }
 
