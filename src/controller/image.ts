b5938e459558d1eec6e6c7d82d57de6e311fc44d
CharmingCheol <toby0806@naver.com>
2022-11-10 23:10:58 +0900

    multerOption 제거 후 ImageExtensionCheckerPipe 적용

--- src/controller/image.ts
+++ src/controller/image.ts
@@ -1,24 +1,27 @@
 import { Controller, HttpCode, HttpStatus, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { ApiTags, ApiOperation, ApiResponse } from '@nestjs/swagger';
-import { ImageUploadResponseDto } from 'dto/image';
 import { Express } from 'express';
+
+import { ImageUploadResponseDto } from 'dto/image';
 import FileNullValiationPipe from 'pipe/file-null-validation.pipe';
+import ImageExtensionCheckerPipe from 'pipe/image-extension-checker.pipe';
 import ImageService from 'service/image';
-import multerOptions from 'utils/multerStorage';
 
 @Controller('images')
 @ApiTags('Images API')
 class ImageController {
   constructor(private imageService: ImageService) {}
 
-  @Post('writing')
+  @Post('writings/contents')
   @HttpCode(HttpStatus.OK)
-  @UseInterceptors(FileInterceptor('writing', multerOptions))
+  @UseInterceptors(FileInterceptor('writing'))
   @ApiOperation({ summary: '게시글 이미지 업로드' })
   @ApiResponse({ status: HttpStatus.OK, type: ImageUploadResponseDto })
-  uploadWritingImage(@UploadedFile(new FileNullValiationPipe()) file: Express.Multer.File) {
-    return { path: this.imageService.createImageURL(file.filename) };
+  uploadWritingContentImage(
+    @UploadedFile(new FileNullValiationPipe(), new ImageExtensionCheckerPipe()) file: Express.Multer.File,
+  ) {
+    return { path: this.imageService.createImageURL(file) };
   }
 }
 
