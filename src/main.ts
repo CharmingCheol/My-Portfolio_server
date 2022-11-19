8d988c1cc94ade43b0d8ac6d91743362d0a9f8e9
CharmingCheol <toby0806@naver.com>
2022-11-19 23:49:34 +0900

    로컬에 이미지 추가시키는 로직 제거

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,11 @@
 import { NestFactory } from '@nestjs/core';
-import * as express from 'express';
-import { join } from 'path';
 import swaggerSetup from 'utils/swaggerSetup';
 import AppModule from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: { origin: 'http://localhost:3000' } });
   app.setGlobalPrefix('api');
-  app.use('/images', express.static(join(__dirname, '../images')));
   swaggerSetup(app);
-
   await app.listen(3001);
 }
 
