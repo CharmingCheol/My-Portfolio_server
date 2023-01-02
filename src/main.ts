84b7e0f7bb8c00112a8a8298211b5df24a5f7bff
CharmingCheol <toby0806@naver.com>
2023-01-02 23:45:14 +0900

    cors list에 front url 등록

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,9 @@ import swaggerSetup from 'utils/swaggerSetup';
 import AppModule from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: { origin: 'http://localhost:3000' } });
+  const app = await NestFactory.create(AppModule, {
+    cors: { origin: ['http://localhost:3000', 'https://www.charmingcheol.com/'] },
+  });
   app.setGlobalPrefix('api');
   swaggerSetup(app);
   await app.listen(3001);
