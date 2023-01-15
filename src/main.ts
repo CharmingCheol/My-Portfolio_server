647b791986e3238498ed79c7f1c86caf0ddbda8c
CharmingCheol <toby0806@naver.com>
2023-01-15 13:54:38 +0900

    front 배포 주소 url 마지막에 슬래시 제거

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import AppModule from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
-    cors: { origin: ['http://localhost:3000', 'https://www.charmingcheol.com/'] },
+    cors: { origin: ['http://localhost:3000', 'https://www.charmingcheol.com'] },
   });
   app.setGlobalPrefix('api');
   swaggerSetup(app);
